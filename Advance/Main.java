f576c770129d2aa2c5a81ad6b6e12ac27a88c834
Amaan <89463628+Ammo-Nation@users.noreply.github.com>
2022-01-13 19:50:44 +0530

    Add files via upload

---
+++ Advance/Main.java
@@ -0,0 +1,52 @@
+package com.codus;
+
+public class Main {
+//Object-Oriented Programming tries to map code instructions with real-world, making the code short and easier to
+// understand.
+//With the help of OOPs, we try to implement real-world entities such as object, inheritance, abstraction, etc.
+//OOPs helps us to follow the DRY(Don't Repeat Yourself) approach of programming, which in turn increases the
+// reusability of the code.
+
+
+
+//    Two most important aspects of OOPs - Classes & Objects :
+/*
+Class :
+A class is a blueprint for creating objects.
+Classes do not consume any space in the memory.
+Objects inherit methods and variables from the class.
+It is a logical component.
+
+Objects :
+An object is an instantiation of a class. When a class is defined, a template (info) is defined.
+Every object has some address, and it occupies some space in the memory.
+It is a physical entity.
+
+
+We identify the following:
+
+Noun              - Class                        - Employee
+Adjective        - Attributes                  - name, age, salary
+Verb               - Methods                    - getSalary(), increment()
+
+OOPS TERMINOLOGY
+Abstraction-
+    Hiding Internal Details
+
+Encapsulation-
+    In java , encapsulation means hiding the sensitive data from users
+
+Inheritance-
+    The act of deriving new things from existing things
+
+Polymorphism-
+    One entity, Many forms
+
+
+
+
+ */
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
